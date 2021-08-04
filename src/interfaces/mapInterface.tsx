a93c18108c3a0c16ede7fd915406e4fe3854f465
luckmer <woszczo04@gmail.com>
2021-08-04 15:03:28 +0200

    interfaces

---
+++ src/interfaces/mapInterface.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export interface mapInterface {
+  id: number;
+  name: number;
+}
+export interface imageInterface {
+  img: string;
+  id: number;
+}
+export interface clickInterface {
+  id: number;
+  name: number;
+}
+export interface pairInterface {
+  first: number;
+  second: number;
+}
+
+export interface MainGameInterface {
+  image: imageInterface[];
+  id: number;
+  click: clickInterface[];
+  name: number;
+  pair: pairInterface[];
+  checkPair: (item: pairInterface, name: number, open: boolean) => boolean;
+  i: number;
+  handleClick: (event: React.MouseEvent<HTMLElement>) => void;
+}
